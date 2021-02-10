3c44e33ea4681a8b72404be150cba28bff9d4788
Alex <bozeak@gmail.com>
2021-02-10 15:38:15 +0200

    EASYOAPC-1273 - Use OPAC url from easyopac_redirect module.

--- templates/bibliofil_lists_item.tpl.php
+++ templates/bibliofil_lists_item.tpl.php
@@ -8,6 +8,6 @@
  * @var array $data
  */
 ?>
-<a href="<?php print $opac_url . '/cgi-bin/m2?tnr=' . $data['id']; ?>">
+<a href="<?php print $opac_url . $data['id']; ?>">
   <img src="<?php print $data['cover_src']; ?>" alt="<?php print $data['title']; ?>">
 </a>
